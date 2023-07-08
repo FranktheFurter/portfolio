132c6040be16f2b3181093779b690baabde00e3a
Marc Frankfurter <johndoe@example.commarcfrankfurter@gmail.com>
2023-07-08 10:41:57 +0200

    improve gallery

--- components/ImageGallery.vue
+++ components/ImageGallery.vue
@@ -49,27 +49,22 @@ const prevImage = () => {
     <div
       v-if="selectedIndex !== null"
       class="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center"
+      @click="closeImage"
     >
       <div
-        class="absolute top-4 left-4 text-2xl text-white cursor-pointer"
-        @click="closeImage"
-      >
-        X
-      </div>
-      <div
         class="absolute top-20% bottom-20% left-0 text-2xl text-white flex items-center px-4 cursor-pointer"
-        @click="prevImage"
+        @click.stop="prevImage"
       >
         ←
       </div>
       <img
         :src="$config.app.baseURL + props.imgUrls[selectedIndex]"
         alt="Full Screen Image"
-        class="max-w-4/5 max-h-4/5"
+        class="max-w-100% max-h-100% md:max-w-4/5 md:max-h-4/5"
       />
       <div
         class="absolute top-20% bottom-20% right-0 text-2xl text-white flex items-center px-4 cursor-pointer"
-        @click="nextImage"
+        @click.stop="nextImage"
       >
         →
       </div>
